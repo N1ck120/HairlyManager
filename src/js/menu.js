847b58645e9df5f35f78bd77a682fd8fa6f10ddf
pedrohrqphp <pedrohrq2005@gmail.com>
2023-10-20 22:36:12 -0300

    Adição da NavBar

---
+++ src/js/menu.js
@@ -0,0 +1,11 @@
+function showMenu() {
+    let menuMobile = document.querySelector('.mobile-menu');
+
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = 'img/menu.png'
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = 'img/close.png'
+    }
+}
