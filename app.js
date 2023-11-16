37c381608f2b48654746428d36f61cd655a5a7d9
N1ck120 <nickolasaugusto74@gmail.com>
2023-11-15 23:46:21 -0300

    Finalizado sistema de login com JWT

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
 const app = express();
 var path = require('path');
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const port = 8081
+
+//Geração da chave de assinatura do token
+
+const secret = crypto.randomBytes(64).toString('hex');
+console.log(secret);
+
+//Funções de verificação e decodificação de token
+
+function verifyToken(req, res, next) {
+  const token = req.cookies['token'];
+  if (!token) {
+      return res.status(403).redirect('/401');
+  }
+  jwt.verify(token, secret, function(err, decoded) {
+      if (err) {
+          // Redirecionar para a página 401
+          return res.status(401).redirect('/401');
+      }
+      req.userId = decoded.id;
+      next();
+  });
+}
+
+function decodeToken(token) {
+  const decoded = jwt.decode(token);
+  console.log(decoded);
+}
 
 //Rotas de redirecionamento
 
@@ -23,8 +54,9 @@ app.get('/finance', function(req, res) {
     res.sendFile(__dirname + '/finance.html');
 });
   
-app.get('/index', function(req, res) {
+app.get('/index',verifyToken, function(req, res) {
     res.sendFile(__dirname + '/index.html');
+    decodeToken(req.cookies['token']);
 });
   
 app.get('/login', function(req, res) {
@@ -105,27 +137,48 @@ app.post('/signup', async (req, res) => {
 app.post('/login', (req, res) => {
   const email = req.body.email;
   const pass = req.body.pass;
-  connection.query('SELECT pass FROM usr WHERE email = ?', [email], async (error, users) => {
+  connection.query('SELECT * FROM usr WHERE email = ?', [email], async (error, users) => {
     if (error) {
       return res.json({ success: false, message: 'Database query failed' });
     }
     if (users.length === 0) {
       console.log("Email não cadastrado")
-      return res.json({ success: false });
-      
+      return res.json({ success: 1 });
     }
     const user = users[0];
     if (await argon2.verify(user.pass, pass)) {
-      console.log("Logado")
-      res.json({ success: true });
-      
+
+      const token = jwt.sign({ id: user.id_usr, name: user.nm_usr, email: email }, secret, { expiresIn: '1h' });
+      res.json({ success: 0, token: token });
+      console.log("Logado");
+
     } else {
-      console.log("Senha incorreta")
-      res.json({ success: false });
+
+      console.log("Senha incorreta");
+      res.json({ success: 2 });
+      
     }
   });
 });
 
+
+
+app.use((req, res, next) => {
+  // Obtenha o token do cabeçalho de autorização
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) return res.sendStatus(401); // Se não houver token, retorne um erro
+
+  // Verifique a validade do token
+  jwt.verify(token, secret, (err, user) => {
+    if (err) return res.sendStatus(403); // Se o token não for válido, retorne um erro
+
+    req.user = user; // Salve as informações do usuário na solicitação
+
+    next(); // Continue para a próxima rota
+  });
+});
 
 
 
--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,16 @@
 // Função para fazer a requisição HTTP
+function criarCookie(nome, valor, dias) {
+  if (dias) {
+      var data = new Date();
+      data.setTime(data.getTime() + (dias * 24 * 60 * 60 * 1000));
+      var expira = "; expires=" + data.toGMTString();
+  } else {
+      var expira = "";
+  }
+  document.cookie = nome + "=" + valor + expira + "; path=/";
+}
+
+
 function login() {
   let email = document.getElementById('email').value.toLowerCase();
   let pass = document.getElementById('pass').value;
@@ -22,14 +34,22 @@ function login() {
       .then(response => response.json()) // transforma a resposta em JSON
       .then(data => {
         // Adicione suas ações aqui dependendo da resposta
-        if(data.success) {
-            console.log("secesso"+data.sucess)
-            console.log(data)
+        if(data.success == 0) {
+
+          document.getElementById("errormsg").style.display = "none";
+          criarCookie("token", data.token, 24);
+          window.location.replace("/index");
+
           // Ação se a resposta for bem-sucedida
-        } else {
-            console.log("secesso"+data.sucess)
-            console.log("dados"+data)
+        } else if(data.success == 2){
+
+            console.log("dados: "+data)
+            document.getElementById("errormsg").style.display = "block";
+            document.getElementById("errormsgtxt").textContent= "Erro! Senha incorreta!";
           // Ação se a resposta não for bem-sucedida
+        }else{
+          document.getElementById("errormsg").style.display = "block";
+          document.getElementById("errormsgtxt").textContent= "Erro! Email não cadastrado!";
         }
       })
       .catch((error) => {
