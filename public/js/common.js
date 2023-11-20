4ad03f7b772d05126e4b4a16808c7a34d574fdb1
N1ck120 <nickolasaugusto74@gmail.com>
2023-11-19 22:35:12 -0300

    Atualização nos botões mostrar senha

--- public/js/common.js
+++ public/js/common.js
@@ -1,31 +1,43 @@
+var isChecked = true;
+
 function showpass() {
-    var pass1 = document.getElementById("pass");
-    var pass2 = document.getElementById("pass1");
-    var showPass = document.getElementById("showpass");
-  
-    if(showPass.checked) {
-      pass1.type = "text";
-      pass2.type = "text";
-    }else{
-      pass1.type = "password";
-      pass2.type = "password";
-    }
+  if(isChecked) {
+    document.getElementById("eye").src = "/assets/bootstrap-icons/eye.svg";
+    document.getElementById("pass").type = "text";
+    isChecked = false;
+  } else {
+    document.getElementById("eye").src = "/assets/bootstrap-icons/eye-slash.svg";
+    document.getElementById("pass").type = "password";
+    isChecked = true;
   }
+}
 
 
-  function showpass1() {
-    var pass1 = document.getElementById("pass");
-    var pass2 = document.getElementById("pass1");
-    var pass3 = document.getElementById("pass2");
-    var showPass = document.getElementById("showpass");
-  
-    if(showPass.checked) {
-      pass1.type = "text";
-      pass2.type = "text";
-      pass3.type = "text";
-    }else{
-      pass1.type = "password";
-      pass2.type = "password";
-      pass3.type = "password";
-    }
+var isChecked1 = true;
+
+function showpass1() {
+  if(isChecked1) {
+    document.getElementById("eye1").src = "/assets/bootstrap-icons/eye.svg";
+    document.getElementById("pass1").type = "text";
+    isChecked1 = false;
+  } else {
+    document.getElementById("eye1").src = "/assets/bootstrap-icons/eye-slash.svg";
+    document.getElementById("pass1").type = "password";
+    isChecked1 = true;
   }
+}
+
+
+var isChecked2 = true;
+
+function showpass2() {
+  if(isChecked2) {
+    document.getElementById("eye2").src = "/assets/bootstrap-icons/eye.svg";
+    document.getElementById("pass2").type = "text";
+    isChecked2 = false;
+  } else {
+    document.getElementById("eye2").src = "/assets/bootstrap-icons/eye-slash.svg";
+    document.getElementById("pass2").type = "password";
+    isChecked2 = true;
+  }
+}
